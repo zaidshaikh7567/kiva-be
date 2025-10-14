a3e2cbbc109754648ac384c776f545e6e27adcdd
Zaid Shaikh <zaidshaikh7567@gmail.com>
2025-10-14 18:31:29 +0000

    Server setup changed

---
+++ src/config/logger.js
@@ -0,0 +1,27 @@
+const winston = require('winston');
+const ENV = require('./env');
+
+const logger = winston.createLogger({
+  level: ENV.NODE_ENV === 'production' ? 'info' : 'debug',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.errors({ stack: true }),
+    winston.format.json()
+  ),
+  defaultMeta: { service: 'kiva-be' },
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
+
+if (ENV.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple()
+    )
+  }));
+}
+
+module.exports = logger;
