b85b49eac669cce3c81b823a72e68eb442b0e141
Ridhdhi-Avaiya <avaiyaridhdhi@gmail.com>
2025-11-11 18:42:06 +0530

    review API changes for backend issue

--- backend/middleware/validate.js
+++ backend/middleware/validate.js
@@ -1,11 +1,16 @@
 const validate = (schema, type = 'body') => {
   return (req, res, next) => {
     try {
-      const source = req[type];
+      const source = req[type] ;
       if (!source) {
         throw new Error(`Invalid source: ${type} not found on request`);
       }
       req[type] = schema.parse(source);
+      // const source = req[type];
+      // const parsingTarget =
+      //   source === undefined || source === null ? {} : source;
+
+      // req[type] = schema.parse(parsingTarget);
       next();
     } catch (error) {
       next(error);
